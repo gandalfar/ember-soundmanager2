f96d83fbae150ae975dafb60bda592e81cf163de
Jure Cuhalev <jure@cuhalev.com>
2017-02-05 17:52:15 +0100

    Initial commit

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
 /* eslint-env node */
 'use strict';
 
+const path = require('path');
+const mergeTrees = require('broccoli-merge-trees');
+const Funnel = require('broccoli-funnel');
+
 module.exports = {
-  name: 'ember-soundmanager2'
+  name: 'ember-soundmanager2',
+
+  included: function(target) {
+    this._super.included.apply(this, arguments);
+
+    const app = target.app || target;
+    const sm2Path = 'vendor/soundmanager2';
+
+    app.import(path.join(sm2Path, 'soundmanager2-nodebug.js'));
+  },
+
+  treeForVendor: function(vendorTree) {
+    let trees = [];
+    let sm2Path = path.join(path.dirname(require.resolve('soundmanager2'), 'script'));
+
+    if (vendorTree) {
+      trees.push(vendorTree);
+    }
+
+    trees.push(new Funnel(sm2Path, {
+      destDir: 'soundmanager2'
+    }));
+
+    return mergeTrees(trees);
+  }
+
 };
